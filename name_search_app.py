f597c6199366d1f9b199c7b60417de1d111ba45c
kyle <kyle_hoho@foxmail.com>
2025-02-13 16:30:02 +0800

    Initial commit of flask name search project

--- name_search_app.py
+++ name_search_app.py
@@ -1,38 +1,28 @@
-import pandas as pd
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-
-def load_data():
-    try:
-        df = pd.read_excel('C:\\Users\\DELL\\Desktop\\2025年会\\数据表1.xlsx')
-        print('数据全部内容信息：')
-        print(df.to_csv(sep='\t', na_rep='nan'))
-        data = df.values.flatten().tolist()
-        return data
-    except FileNotFoundError:
-        print('C:\\Users\\DELL\\Desktop\\2025年会\\数据表1.xlsx 文件未找到')
-        return []
-
-
-table_data = load_data()
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/search')
-def search():
-    query = request.args.get('name')
-    global table_data
-    print('当前使用的 table_data:', table_data)
-    results = [name for name in table_data if query.lower() in name.lower()]
-    print('搜索结果 results:', results)
-    return render_template('index.html', results=results)
-
-
-if __name__ == '__main__':
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.form.get('keyword')
+    try:
+        df = pd.read_excel('/mnt/数据表1.xlsx')
+        print('读取到的数据基本信息：')
+        df.info()
+        print('读取到的数据内容信息：')
+        print(df.to_csv(sep='\t', na_rep='nan'))
+        results = df[df['姓名'].str.contains(keyword, na=False)]
+        print('匹配结果：')
+        print(results.to_csv(sep='\t', na_rep='nan'))
+    except FileNotFoundError:
+        results = pd.DataFrame(columns=['姓名', '出发日期', '出发时间', '班次'])
+        print("未找到数据表1.xlsx文件，请检查文件路径。")
+    return render_template('search.html', results=results)
+
+if __name__ == '__main__':
     app.run(debug=True)
